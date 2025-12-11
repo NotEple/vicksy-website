003f5c2535b584c7c9802aa5ff49bbe6fa99de1f
Erlend Svanevik <erlendms@hotmail.no>
2025-12-11 21:47:31 +0100

    small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import Dashboard from "./pages/Dashboard";
-import Socials from "./pages/Socials";
-import Error from "./pages/Error";
-import Landing from "./pages/Landing";
+// import Dashboard from "./pages/Dashboard";
+// import Socials from "./pages/Socials";
+// import Error from "./pages/Error";
+// import Landing from "./pages/Landing";
 import TitleController from "./components/TitleController";
 import vicksyPeek from "./assets/vicksyPeek.png";
 import vicksyFastRoll from "./assets/vicksyFastRoll.gif";
+import Development from "./pages/Development";
 
 function App() {
   return (
@@ -16,13 +17,13 @@ function App() {
       <TitleController />
       <div className="w-full h-dvh flex flex-col bg-bg-light">
         <Navbar />
-        <main className="h-full relative overflow-hidden">
+        <main className="h-full relative overflow-hidden items-center flex justify-center w-full">
           <Routes>
-            <Route path="/" element={<Landing />} />
+            <Route path="/" element={<Development />} />
             {/* <Route path="/merch" element={<Merch />} /> */}
-            <Route path="/socials" element={<Socials />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="*" element={<Error />} />
+            <Route path="/socials" element={<Development />} />
+            <Route path="/dashboard" element={<Development />} />
+            <Route path="*" element={<Development />} />
           </Routes>
 
           <img src={vicksyPeek} className="absolute top-1/2 -left-6" />
---
+++ src/pages/Development.tsx
@@ -0,0 +1,12 @@
+import vicksyWideConstruction from "../assets/vicksyWideConstruction.png";
+
+function Development() {
+  return (
+    <div className="bg-neutral-800 text-3xl gap-8 rounded-lg justify-center px-8 pt-8 flex items-center w-fit h-fit flex-col">
+      WEBSITE IS UNDER CONSTRUCTION
+      <img src={vicksyWideConstruction} />
+    </div>
+  );
+}
+
+export default Development;
