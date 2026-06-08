cf3ed94eaf834a47b4cbab4cb47bfd0e5de3e781
Erlend Svanevik <erlendms@hotmail.no>
2026-06-09 00:42:14 +0200

    feat: Changed title and favicon based on status.

--- src/components/TitleController.tsx
+++ src/components/TitleController.tsx
@@ -1,20 +1,28 @@
 import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { useLive } from "@/hooks/useLive";
+import vicksyWLIVE from "/vicksyWLIVE.png";
 
 export default function TitleController() {
   const location = useLocation();
-
-  const routes = [
-    { path: "/", title: "Home | Vicksy" },
-    { path: "/merch", title: "Merch | Vicksy" },
-    { path: "/socials", title: "Socials | Vicksy" },
-    { path: "/dashboard", title: "Dashboard | Vicksy" },
-  ];
+  const { isLive } = useLive();
 
   useLayoutEffect(() => {
+    const routes = [
+      { path: "/", title: "Home | Vicksy" },
+      { path: "/merch", title: "Merch | Vicksy" },
+      { path: "/socials", title: "Socials | Vicksy" },
+      { path: "/dashboard", title: "Dashboard | Vicksy" },
+    ];
+
+    const favicon = document.getElementById("favicon") as HTMLLinkElement;
+
+    if (isLive) favicon.href = vicksyWLIVE;
+
     const route = routes.find((r) => r.path === location.pathname);
-    if (route?.title) document.title = route.title;
-  });
+    if (route?.title)
+      document.title = `${isLive ? "(LIVE)" : ""} ${route.title}`;
+  }, [isLive, location.pathname]);
 
   return null;
 }
