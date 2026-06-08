0b3ec7212d96965327efb0ddcdaa0bdf4826c544
Erlend Svanevik <erlendms@hotmail.no>
2026-06-09 00:40:34 +0200

    chore: Removed unused code

--- src/components/hooks/useTitleUpdate.tsx
+++
@@ -1,9 +0,0 @@
-import { useLayoutEffect } from "react";
-
-export function useTitleUpdate() {
-  useLayoutEffect(() => {
-    document.title = window.location.pathname + " | " + "Vicksy";
-  }, []);
-
-  return;
-}
